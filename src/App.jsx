a4070ecaa658e87928250314ebf5af2bb4f69122
toshiakifukikoshi <86909359+toshiakifukikoshi@users.noreply.github.com>
2022-04-09 23:54:33 +0900

    4_9途中です

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,31 @@ import React, { useState } from "react";
 import "./style.css";
 
 export const App = () => {
-  const [todoText, setTodoText] = useState();
+  const [todoText, setTodoText] = useState("");
 
   const [incompleteTodos, setIncompleteTodos] = useState([
     "テキスト1",
     "テキスト2",
     "テキスト3"
   ]);
-
-  const onChangeTodoText = (e) => {
-    console.log(e.target.value);
-    setTodoText();
-  };
-
   const [completeTodos, setCompleteTodos] = useState([
     "完了テキスト１",
     "完了テキスト２"
   ]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  const onClickDelete = (index) => {
+    alert(index);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -27,18 +35,18 @@ export const App = () => {
           value={todoText}
           onChange={onChangeTodoText}
         />
-        <button>追加</button>
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了エリア</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <li key={todo}>
                 <div className="list-row">
                   <p>{todo}</p>
                   <button>完了</button>
-                  <button>削除</button>
+                  <button onClick={() => onClickDelete(index)}>削除</button>
                 </div>
               </li>
             );
